79bdd5f36abe1202bc1961357b72cd0e1d5e6ddb
Ujjwal <ujjwalpandey.aps@gmail.com>
2024-03-14 10:26:54 +0530

    first commit, init assisment

---
+++ vitraImageTextExtraction/src/main/java/com/vitraImageTextExtraction/VitraImageTextExtractionApplication.java
@@ -0,0 +1,13 @@
+package com.vitraImageTextExtraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitraImageTextExtractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitraImageTextExtractionApplication.class, args);
+	}
+
+}
